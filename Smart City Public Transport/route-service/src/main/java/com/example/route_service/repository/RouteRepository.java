59e3a5170a499513144029c471416a4451ade6c8
Sunilvarma-l <lakkamrajusunil@gmail.com>
2025-08-23 09:27:39 +0530

    updated

--- Smart City Public Transport/route-service/src/main/java/com/example/route_service/repository/RouteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.route_service.repository;
-
-import com.example.route_service.model.Route;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RouteRepository extends JpaRepository<Route, Long> {
-}
--- Smart City Public Transport/vehicle-tracking-service/src/main/java/com/example/vehicle_tracking_service/repository/VehicleRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.vehicle_tracking_service.repository;
-
-import com.example.vehicle_tracking_service.model.Vehicle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> {}
